6614db2b3868cbdf2d198c6c466ddd04afd7b63b
ReemAl-Salihi <157957138+ReemAl-Salihi@users.noreply.github.com>
2024-03-16 16:03:01 +0300

    Update on register.php

--- register.php
+++ register.php
@@ -72,6 +72,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
 }
 
+function handleFileUpload() {
+    $targetDir = "uploads/";  // Create a folder named "uploads" to store user images
+    $targetFile = $targetDir . basename($_FILES["image"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+    // Check if image file is a actual image or fake image
+    if (isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["image"]["tmp_name"]);
+        if ($check !== false) {
+            $uploadOk = 1;
+        } else {
+            $error = "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        $error = "Sorry, your file was not uploaded.";
+    } else {
+        // If everything is ok, try to upload file
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
+            return $targetFile;
+        } else {
+            $error = "Sorry, there was an error uploading your file.";
+        }
+    }
+
+    return 'uploads/default.png'; // Return default image path if there's an error
+}
+
 
 
 ?>
